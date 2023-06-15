d08f202f0c1a8bf85c1c0e33a0dba13d433c99bc
Mario Ahumada <ahumadamob@gmail.com>
2023-06-15 20:21:47 -0300

    Circuito basico listo

---
+++ src/main/java/imb3/progra3/entity/Producto.java
@@ -0,0 +1,42 @@
+package imb3.progra3.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Producto {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private Double precio;
+	private boolean disponible;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	
+}
