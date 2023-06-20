6b0e862565d8faa98ca5b4395638744d350d1982
Arrigo <arrigo.z.97@gmail.com>
2023-06-20 11:13:02 -0300

    Arreglada dependencia para que funcione sobre todo el documento, añadidas anotaciones a entidad.

--- src/main/java/imb3/progra3/entity/CiaDeSeguros.java
+++ src/main/java/imb3/progra3/entity/CiaDeSeguros.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class CiaDeSeguros {
@@ -11,8 +14,13 @@ public class CiaDeSeguros {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@NotBlank(message = "El nombre no puede estar vacío.")
+	@Size(max=50, message = "El nombre no puede tener más de 50 caracteres.")
 	private String nombre;
+	@NotBlank(message = "La dirección no puede estar vacía.")
+	@Size(min = 3, max = 100, message = "Mínimo 3, máximo 100 caracteres.")
 	private String direccion;
+	@Pattern(regexp = "\\d{10}")
 	private Integer numTelefono;
 	private String tipoSeguro; //terceros, total, empresarial
 	private boolean estadoSeguro; //0= baja, 1= alta
