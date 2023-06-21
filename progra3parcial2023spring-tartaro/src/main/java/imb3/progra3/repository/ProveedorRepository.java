578544d1650e1fcddb7f199f75f0652597c0143e
Ignacio N. Tártaro <86450461+SLNacho@users.noreply.github.com>
2023-06-21 19:52:14 -0300

    Delete progra3parcial2023spring-tartaro directory

--- progra3parcial2023spring-tartaro/src/main/java/imb3/progra3/repository/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package imb3.progra3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import imb3.progra3.entity.Proveedor;
-
-public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
-
-}
