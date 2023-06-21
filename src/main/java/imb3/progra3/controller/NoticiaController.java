68264038bcffebdeae55d92e7b7fe8f964394e9c
Luis David <luisda8193@hotmail.com>
2023-06-21 19:10:04 -0300

    Mejoré controladores

--- src/main/java/imb3/progra3/controller/NoticiaController.java
+++ src/main/java/imb3/progra3/controller/NoticiaController.java
@@ -1,8 +1,11 @@
 package imb3.progra3.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,30 +26,99 @@ public class NoticiaController {
 	INoticiaService service;
 	
 	@GetMapping("/noticia")
-	public List<Noticia>buscarTodos(){
-		return service.buscarTodos();
+	public ResponseEntity<APIResponse<List<Noticia>>>buscarTodos(){
+		APIResponse<List<Noticia>> response = new APIResponse<List<Noticia>> (200, null, service.buscarTodos());
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
 	@GetMapping("/noticia/{id}")
-	public Noticia buscarPorId(@PathVariable("id") Integer id) {
-		return service.buscarPorId(id);
+	public ResponseEntity<APIResponse<Noticia>> buscarPorId(@PathVariable("id") Integer id) {
+		Noticia noticia = service.buscarPorId(id);
+		if(noticia == null){
+			List<String> messages = new ArrayList<>();
+			messages.add("No se encontró la Noticia con id = " + id.toString());
+			messages.add("Revise el parámetro");
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.BAD_REQUEST.value(), messages, null);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}else {
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.OK.value(),null,noticia);
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+		}
 	}
 	
 	@PostMapping("/noticia")
-	public Noticia crearNoticia(@RequestBody Noticia noticia) {
+	public ResponseEntity<APIResponse<Noticia>> crearNoticia(@RequestBody Noticia noticia) {
+		if(noticia.getId() != null) {
+			Noticia buscaNoticia = service.buscarPorId(noticia.getId());
+			if (buscaNoticia != null) {
+				List<String> messages = new ArrayList<>();
+				messages.add("Ya existe una noticia con el ID = " + noticia.getId().toString());
+				messages.add("Para actualizar utilice el verbo PUT");
+				APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.BAD_REQUEST.value(), messages, null);
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+			}
+		}
 		service.guardar(noticia);
-		return noticia;
+		APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.CREATED.value(),null,noticia);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+		
 	}
 	
 	@PutMapping("/noticia")
-	public Noticia actualizarNoticia(@RequestBody Noticia noticia) {
-		service.guardar(noticia);
-		return noticia;
+	public ResponseEntity<APIResponse<Noticia>> actualizarNoticia(@RequestBody Noticia noticia) {
+		boolean isError;
+		String idStr;
+		if(noticia.getId() != null) {
+			Noticia buscaNoticia = service.buscarPorId(noticia.getId());
+			idStr = noticia.getId().toString();
+			if (buscaNoticia != null) {
+				//Devolver un OK
+				isError = false;
+			}else {
+				//Devolver un Error
+				isError = true;
+			}
+			
+		}else {
+			idStr = "<No definido>";
+			//Devolver un error
+			isError = true;
+		}
+		
+		if(isError) {
+			//Devolvermos el error
+			List<String> messages = new ArrayList<>();
+			messages.add("No existe una noticia con el ID = " + idStr);
+			messages.add("Para crear una nueva utilice el verbo POST");
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.BAD_REQUEST.value(), messages, null);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}else {
+			//Devolvemos el OK
+			service.guardar(noticia);
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.OK.value(),null,noticia);
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+		}
+		
 	}
 	
 	@DeleteMapping("/noticia/{id}")
-	public void eliminar(@PathVariable("id") Integer id) {
-		service.eliminar(id);
+	public ResponseEntity<APIResponse<Noticia>> eliminar(@PathVariable("id") Integer id) {
+		Noticia buscaNoticia = service.buscarPorId(id);
+		if(buscaNoticia == null) {
+			//Error
+			List<String> messages = new ArrayList<>();
+			messages.add("No existe una noticia con el ID = " + id.toString());
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.BAD_REQUEST.value(),messages,null);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}else {
+			service.eliminar(id);
+			List<String> messages = new ArrayList<>();
+			messages.add("La noticia que figura en el cuerpo ha sido eliminada");
+			APIResponse<Noticia> response = new APIResponse<Noticia>(HttpStatus.OK.value(),messages,buscaNoticia);
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+			//eliminar
+		}
+		
 	}
 
 }
