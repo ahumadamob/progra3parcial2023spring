446c56914ff3f44e3e08f44ac5c366ff910eaab7
fernando <fdbarroso@gmail.com>
2023-06-22 20:45:26 -0300

    arreglos al proyecto

--- src/main/java/com/libros/ibm/libros/controlador/Controlador.java
+++ src/main/java/com/libros/ibm/libros/controlador/Controlador.java
@@ -1,4 +1,7 @@
 package com.libros.ibm.libros.controlador;
+import com.libros.ibm.libros.entidades.*;
+import com.libros.ibm.libros.servicio.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.PutExchange;
 
-import com.libros.ibm.libros.InterfazLibro;
-import com.libros.ibm.libros.entidades.Libro;
+
+
 
 @RestController
 @RequestMapping("/api/v1")
