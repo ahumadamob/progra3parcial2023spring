da3a14c6689ac3db65c6cff30450c64c267fca1a
CarPes <pesacarlos1995@gmail.com>
2023-06-28 20:51:13 -0300

    Parcial Progra3

--- progra3parcial2023spring-master/src/main/java/imb3/progra3/Repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package imb3.progra3.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import imb3.progra3.entity.comentario;
-
-public interface ComentarioRepository extends JpaRepository<comentario, Integer>  {
-
-}
